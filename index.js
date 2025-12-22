12c17ccd9f62cf8d0dfb0eb8e7a8a74dfc54873f
Vinesh-dev <kingvinesh56@gmail.com>
2025-12-22 20:09:26 +0530

    Initial backend setup with Express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("CollabNote backend is running 🚀");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
